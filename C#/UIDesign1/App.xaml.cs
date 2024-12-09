ea237c2e603d00c2187e257f2db5e420fcd18e10
a407259222@163.com <407259222@qq.com>
2024-12-09 15:03:33 +0800

    add file

---
+++ C#/UIDesign1/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Windows;
+using UIDesign.utils;
+
+namespace UIDesign
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // 初始化数据库
+            DBManger dbInitializer = new DBManger();
+            dbInitializer.InitializeDatabase();
+        }
+    }
+
+}
